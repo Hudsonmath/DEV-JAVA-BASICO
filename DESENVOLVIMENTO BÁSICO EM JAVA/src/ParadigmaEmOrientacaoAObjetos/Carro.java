3f7b26b733f7bf7d9687b0a7856a66b8d71b83ec
Hudsonmath <hudsonmatheus.09@gmail.com>
2021-04-19 18:39:46 -0300

    Executando Exercícios de O.O

---
+++ DESENVOLVIMENTO BÁSICO EM JAVA/src/ParadigmaEmOrientacaoAObjetos/Carro.java
@@ -0,0 +1,12 @@
+package ParadigmaEmOrientacaoAObjetos;
+
+public class Carro {
+	
+	/*
+	 * Crie uma classe Carro. Nessa classe você deverá ter a 
+	 * quantidade de pessoas que estão dentro do carro. E também 
+	 * é preciso que tenha uma forma de acidionar e remover pessoas 
+	 * dentro do carro.
+	 */
+
+}
