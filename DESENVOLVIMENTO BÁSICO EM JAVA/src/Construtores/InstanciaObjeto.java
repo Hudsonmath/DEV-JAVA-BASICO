e20c5cb5b9e3e0c41f1ed576f284aaf3df0689b2
Hudsonmath <hudsonmatheus.09@gmail.com>
2021-04-20 16:43:22 -0300

    Realização Exercício  objetos com Construtores

---
+++ DESENVOLVIMENTO BÁSICO EM JAVA/src/Construtores/InstanciaObjeto.java
@@ -0,0 +1,21 @@
+package Construtores;
+
+public class InstanciaObjeto {
+	
+	public static void main(String[] args) {
+		
+		
+		Carro novoCarro = new Carro(" chevrolet "," corsa ", 2000);
+		novoCarro.variante = " 4 Portas";
+		
+		novoCarro.getCarro();	
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
